c4d997f9e3a9ef3b9d9f829dc872e6add8d76ff8
Joli Holmes <holmesjoli@gmail.com>
2023-02-09 18:46:05 -0500

    wrote function to update date

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function updateDate() {
+
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+    var today = new Date();
+    var dd = today.getDate();
+    var yyyy = today.getFullYear();
+
+    let day = weekday[today.getDay()];
+    let month = months[today.getMonth()]
+
+    d3.select("#date").text(`${day}, ${month} ${dd} ${yyyy}`)
+}
+
+
+updateDate()
