4064a02171672a9b5c6360e0c2728eb38867fc20
Joli Holmes <holmesjoli@gmail.com>
2023-02-14 08:14:27 -0500

    made separate script files to keep everything organized

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-function updateDate() {
-
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-
-    var today = new Date();
-    var dd = today.getDate();
-    var yyyy = today.getFullYear();
-
-    let day = weekday[today.getDay()];
-    let month = months[today.getMonth()]
-
-    d3.select("#date").text(`${day}, ${month} ${dd} ${yyyy}`)
-}
 
 function allowDrop(ev) {
     ev.preventDefault();
@@ -28,4 +14,3 @@ function drop(ev) {
     ev.target.appendChild(document.getElementById(data));
 }
 
-updateDate();
