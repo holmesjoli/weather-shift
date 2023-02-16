7a71c3457c7b553d9f74db9087cb9c48b9690ac5
Joli Holmes <holmesjoli@gmail.com>
2023-02-15 20:52:25 -0500

    randomized data

--- js/drag-and-drop.js
+++ js/drag-and-drop.js
@@ -14,20 +14,38 @@ function drop(ev) {
     ev.target.appendChild(document.getElementById(data));
 }
 
+// shuffle Array
+// from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
 //
 function addTerms(data) {
     let container = d3.select("#drag-container");
 
+    let rowi = Array(data.length).fill(0).map(Number.call, Number);
+    shuffleArray(rowi)
+
+    let data2 = []
+    for (let i of rowi) {
+        data2.push(data[i])
+    }
+
     let groups = container.selectAll("div")
-        .data(data)
+        .data(data2)
         .join("g")
         .attr("class", "drag-item")
         .attr("id", d => "drag"+d.id)
         .attr("draggable", "true")
         .attr("ondragstart", "drag(event)");
 
-
-    console.log(data)
+    // console.log(data)
     groups
         .append("img")
         .attr("src", d => `assets/images/${d.folder}/${d.gif}.gif`)
